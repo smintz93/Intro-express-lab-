8763475b1a9e7327527dc2d4450b153f0f0b7fb5
Sam <sammintz1993@gmail.com>
2018-04-09 12:01:42 -0500

    Commit 2:  Magic 8 Ball express application created.

--- express_lab.js
+++ express_lab.js
@@ -36,9 +36,25 @@ app.get("/greeting/:name", (req, res) => {
 
 
 app.get("/tip/:total/:tipPercentage", (req, res) => {
-	res.send(req.params.total / req.params.tipPercentage)
+	res.send(req.params.tipPercentage)
 });
 
+
+
+
+
+// Magic 8 Ball
+
+const resp = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+const answer = function(){
+		return Math.floor(Math.random()*resp.length)
+	}
+
+app.get("/magic/Will%20I%20Be%20A%20Millionaire", (req, res) => {
+
+	res.send(resp[answer()])
+})
 
 
 
